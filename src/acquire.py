fd0044fdcf6a76aae76fa79273eb6df4f719ba66
Ryan McCall <ryan.e.mccall@gmail.com>
2020-10-07 11:45:14 -0500

    Create download_data and data exists and add their calls to run

--- src/acquire.py
+++ src/acquire.py
@@ -1,6 +1,20 @@
+import os
+
 # Write supporting functions here
+def download_data():
+    os.system("kaggle competitions download nlp-getting-started -f test.csv -p data/raw/")
+    os.system("kaggle competitions download nlp-getting-started -f train.csv -p data/raw/")
+
+def data_exists():
+    return os.path.isfile("data/raw/test.csv") and os.path.isfile("data/raw/train.csv")
 
 def run():
     print("Acquire: downloading raw data files...")
-    # Write code here
+
+    if data_exists():
+        print("Data already exists")
+    else:
+        download_data()
+        print("Data acquired")
+
     print("Acquire: Completed!")
